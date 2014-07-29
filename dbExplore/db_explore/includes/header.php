316df73bdd36dbb6988af84e41053d2494692cbc
root <root@watermelon.(none)>
2014-07-29 14:49:04 -0700

    removed dbExplore.

--- dbExplore/db_explore/includes/header.php
+++
@@ -1,8 +0,0 @@
-<?PHP
-
-$opening_html="<html><head><link rel ='stylesheet' type = 'text/css' href = 'includes/CSS/style.css'></head><body>";
-$closing_html = "</body></html>";
-$backbutton = "<a id = 'backbutton' href = 'index.php'>Back</a>";
-echo $opening_html;
-include "includes/db_cfg/dbCfg.php";
-?>
